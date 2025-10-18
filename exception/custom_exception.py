400d246cc45308f445fa899091046979e86fe972
Navdeep Singh <navdeepsinghdhangar@gmail.com>
2025-10-17 17:03:49 -0700

    added exception files final

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -1,7 +1,7 @@
 import sys
 import traceback
 from logger.custom_logger import CustomLogger
-logger = CustomLogger().get_logger("exception_experiment")
+logger = CustomLogger().get_logger(__file__)
 class DocumentPortalException(Exception):
     """Custom exception for Document Portal"""
     def __init__(self,error_message:str,error_details:sys):
