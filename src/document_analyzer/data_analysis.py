f26a49e435a03926712036ff289cb806d12f6201
Navdeep Singh <navdeepsinghdhangar@gmail.com>
2025-10-27 18:11:07 -0700

    Added Test.py

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,10 +1,12 @@
 import os
+import sys
 from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
+from prompt.prompt_library import prompt
 
 class DocumentAnalyzer:
     """
@@ -31,5 +33,23 @@ class DocumentAnalyzer:
             raise DocumentPortalException("Error in DocumentAnalyzer initialization", sys)
 
     
-    def analyze_document(self):
-        pass
+    def analyze_document(self,document_text:str)->dict:
+        """Analyze a document's text and rxtract structured mertadata and summary"""
+
+        try:
+            chain = self.prompt | self.llm | self.fixing_parser
+
+            self.log.info("Meta data analyssis chain initialized")
+
+            response = chain.invoke({
+                "format_instructions": self.parser.get_format_instructions(),
+                "document_text": document_text
+            })
+
+            self.log.info("Meta data analysis completed successfully")
+
+            return response
+        
+        except Exception as e:
+            self.log.error(f"Error analyzing document: {e}")
+            raise DocumentPortalException("Error in document analysis", sys)
